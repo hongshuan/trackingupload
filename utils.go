f9547009e841858993bd3784c5f9bf4c68789959
Hanson Li <lihsca@gmail.com>
2018-01-25 17:05:55 -0500

    add utils FileExists

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "os"
     "log"
 )
 
@@ -8,4 +9,36 @@ func CheckError(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+// Exists reports whether the named file or directory exists.
+func FileExists(name string) bool {
+    _, err := os.Stat(name)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return err == nil
+}
+
+func ExistFile(name string) bool {
+    _, err := os.Stat(name)
+    return !os.IsNotExist(err)
+}
+
+func ExistsFile(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return err != nil, err
+}
+
+func FileIsExists(name string) bool {
+    if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		// ??
+    }
+    return true
 }
