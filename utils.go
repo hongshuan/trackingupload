eb0b4f1243e692d9d4d5fa062d9ae7b80738b065
Hongshuan Li <lihsca@gmail.com>
2018-01-31 09:17:07 -0500

    FileExists()

--- utils.go
+++ utils.go
@@ -18,15 +18,15 @@ func CheckError(err error) {
 // Exists reports whether the named file or directory exists.
 func FileExists(name string) bool {
     _, err := os.Stat(name)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return err == nil
+    return !os.IsNotExist(err)
 }
 
 func ExistFile(name string) bool {
     _, err := os.Stat(name)
-    return !os.IsNotExist(err)
+    if os.IsNotExist(err) {
+        return false
+    }
+    return err == nil
 }
 
 func ExistsFile(name string) (bool, error) {
