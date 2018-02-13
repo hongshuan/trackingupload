c13d0b3308cf890d96d61c36ec7da654d88b1ed8
Hanson Li <lihsca@gmail.com>
2018-02-13 17:52:36 -0500

    1 of 50 shades of go

---
+++ ref/shade1.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    data := []string{"one","two","three"}
+
+    for _,v := range data {
+        go func() {
+            fmt.Println(v)
+        }()
+    }
+
+    time.Sleep(3 * time.Second)
+    //goroutines print: three, three, three
+}
+
+//Works:
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    data := []string{"one","two","three"}
+
+    for _,v := range data {
+        vcopy := v //
+        go func() {
+            fmt.Println(vcopy)
+        }()
+    }
+
+    time.Sleep(3 * time.Second)
+    //goroutines print: one, two, three
+}
+
+//Works:
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    data := []string{"one","two","three"}
+
+    for _,v := range data {
+        go func(in string) {
+            fmt.Println(in)
+        }(v)
+    }
+
+    time.Sleep(3 * time.Second)
+    //goroutines print: one, two, three
+}
+
+//Incorrect:
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type field struct {
+    name string
+}
+
+func (p *field) print() {
+    fmt.Println(p.name)
+}
+
+func main() {
+    data := []field{{"one"},{"two"},{"three"}}
+
+    for _,v := range data {
+        go v.print()
+    }
+
+    time.Sleep(3 * time.Second)
+    //goroutines print: three, three, three
+}
+
+//Works:
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type field struct {
+    name string
+}
+
+func (p *field) print() {
+    fmt.Println(p.name)
+}
+
+func main() {
+    data := []field{{"one"},{"two"},{"three"}}
+
+    for _,v := range data {
+        v := v
+        go v.print()
+    }
+
+    time.Sleep(3 * time.Second)
+    //goroutines print: one, two, three
+}
+
+//What do you think you'll see when you run this code (and why)?
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type field struct {
+    name string
+}
+
+func (p *field) print() {
+    fmt.Println(p.name)
+}
+
+func main() {
+    data := []*field{{"one"},{"two"},{"three"}}
+
+    for _,v := range data {
+        go v.print()
+    }
+
+    time.Sleep(3 * time.Second)
+}
+
